487f8c8e37c993ec62aba8dc446b1bce18327e04
nils <contact@nilssoderman.com>
2025-11-10 21:28:16 +0100

    Fixed location of manully copied stubs

--- src/manual/__init__.py
+++ src/manual/__init__.py
@@ -4,7 +4,5 @@ import os
 
 def copy_stubs(target_dir: str):
     current_dir = os.path.dirname(__file__)
-    stubs_path = os.path.join(current_dir, 'stubs')
-    for file in os.listdir(stubs_path):
-        if file.endswith('.pyi'):
-            shutil.copy(os.path.join(stubs_path, file), target_dir)
+    manually_typed_stubs_dir = os.path.join(current_dir, 'stubs')
+    shutil.copytree(manually_typed_stubs_dir, target_dir, dirs_exist_ok=True)
